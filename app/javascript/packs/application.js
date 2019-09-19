3dec95cc4b7bb8e0979e5430ad8ed847eb45eaf3
MarceloFigueira <marcelofigueirapereira@gmail.com>
2019-09-19 16:44:30 -0300

    Estruturando o VueJS

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,11 +1,9 @@
-import Vue from 'vue'
-import App from '../app.vue'
-
+import Vue from 'vue/dist/vue.esm'
+import App from './app.vue'
+ 
 document.addEventListener('DOMContentLoaded', () => {
-  const app = new Vue({
-    render: h => h(App)
-  }).$mount()
-  document.body.appendChild(app.$el)
-
-  console.log(app)
+ const app = new Vue({
+  el: '#app',
+  render: h => h(App)
+ })
 })
